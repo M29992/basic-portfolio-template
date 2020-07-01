947bee9fa98a9342ebeef9ff5e21de5c5c13e76c
Matthew Evans <matt29992@gmail.com>
2020-07-01 19:18:56 +0100

    Update hamburger.js
    added basic commenting

--- JavaScript/hamburger.js
+++ JavaScript/hamburger.js
@@ -1,5 +1,8 @@
+//constant variables
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav_item')
+
+//functions
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
